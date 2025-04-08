2d1e9a2c12db4f5b34b87239ab96292e1ea6db46
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2025-04-08 15:18:56 -0700

    stiuifs

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+"use strict";
 var age = 17;
 var title = "Sabrina";
 var alive = true;
-var greeting = "Welcome to Jurassic Park";
-
+let greeting = "Welcome to Jurassic Park";
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(15234, 3454));
--- index.ts
+++ index.ts
@@ -7,3 +7,5 @@ let greeting : string = "Welcome to Jurassic Park"
 function add(n1: number, n2: number){
     return n1 + n2
 }
+
+console.log(add(15234, 3454))
